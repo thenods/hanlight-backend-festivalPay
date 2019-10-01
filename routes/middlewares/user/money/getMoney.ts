aeef86d9afb39d9b33b43e8dbeaa6ceeb036ea3c
kb4ek <kbaek1202@gmail.com>
2019-10-02 03:30:57 +0900

    show lastApproval when user requested own money

--- routes/middlewares/user/money/getMoney.ts
+++ routes/middlewares/user/money/getMoney.ts
@@ -3,27 +3,36 @@ import { Request, Response, NextFunction } from "express";
 import User from "@Model/user.model";
 import { HanlightUser } from "@Lib/types";
 import CustomError from "@Middleware/error/customError";
+import Charge from "@Model/charge.model";
 
 const getMoney = async (req: Request, res: Response, next: NextFunction) => {
   const hanlightUser: HanlightUser = res.locals.user;
 
   try {
-    const user = await User.findOne({ where: { pk: hanlightUser.pk } });
+    const user = await User.findOne({
+      where: { pk: hanlightUser.pk },
+      include: [
+        {
+          model: Charge,
+          order: [["createdAt", "DESC"]],
+          attributes: ["admin_name"]
+        }
+      ]
+    });
 
     res.json({
       success: true,
       data: {
         user: {
           money: user ? user.money : 0,
-        },
-      },
+          lastApproval: user.changed[0].admin_name
+        }
+      }
     });
-
   } catch (error) {
     console.log(error);
-    next(new CustomError({ name: 'Database_Error' }));
+    next(new CustomError({ name: "Database_Error" }));
   }
 };
 
 export default getMoney;
-
